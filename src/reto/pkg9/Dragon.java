f320072d2b9c1431faa48d04c80aed98cc24e6ec
EduardoGarcia32 <slender560@gmail.com>
2019-05-14 13:59:06 -0500

    Creacion de clase dragon

---
+++ src/reto/pkg9/Dragon.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto.pkg9;
+
+/**
+ *
+ * @author Eduardo Garcia
+ */
+public class Dragon {
+    
+    public String nombre;
+    private int nivelFuego;
+    private int nivelVida;
+    
+        public void SecuestrarUnicornios(String nombre){
+            
+        }
+}
